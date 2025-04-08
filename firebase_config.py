e109e548e60dee53bbad40ff6bbe5ba6cbdb73b4
dmwim-blip <gmorengom@gmail.com>
2025-04-08 10:11:20 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,1 +1,3 @@
+# firebase_config.py
 
+firebase_url = "https://console.firebase.google.com/project/scraper-project-19/database/scraper-project-19-default-rtdb/data/~2F"  # 🔁 Replace with your real Firebase URL
